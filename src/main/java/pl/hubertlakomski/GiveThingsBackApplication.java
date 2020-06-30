1b44303ff5820d22634f65358c12c63a504feebe
Hubert Łakomski <hubertlakomski@gmail.com>
2020-06-30 14:18:37 +0200

    initial commit

---
+++ src/main/java/pl/hubertlakomski/GiveThingsBackApplication.java
@@ -0,0 +1,13 @@
+package pl.hubertlakomski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiveThingsBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiveThingsBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/hubertlakomski/GiveThingsBackApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.hubertlakomski;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GiveThingsBackApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

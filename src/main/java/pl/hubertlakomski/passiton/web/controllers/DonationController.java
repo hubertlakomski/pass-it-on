720da3fef124fb88ab341bf263b3ed4abe312bb0
Hubert Łakomski <hubertlakomski@gmail.com>
2020-07-01 20:45:29 +0200

    creating a DonationController

---
+++ src/main/java/pl/hubertlakomski/passiton/web/controllers/DonationController.java
@@ -0,0 +1,12 @@
+package pl.hubertlakomski.passiton.web.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller("/donation")
+@RequiredArgsConstructor
+public class DonationController {
+
+
+
+}

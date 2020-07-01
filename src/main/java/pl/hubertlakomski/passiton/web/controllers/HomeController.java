9ffe66ccb1a27958bbb85b3b19f238b5f83bead7
Hubert Łakomski <hubertlakomski@gmail.com>
2020-07-01 19:57:05 +0200

    adding the foundation list to the Model in the home page controller

--- src/main/java/pl/hubertlakomski/passiton/web/controllers/HomeController.java
+++ src/main/java/pl/hubertlakomski/passiton/web/controllers/HomeController.java
@@ -1,16 +1,22 @@
 package pl.hubertlakomski.passiton.web.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import pl.hubertlakomski.passiton.service.home.institution.list.InstitutionListHomeService;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final InstitutionListHomeService institutionListHomeService;
 
     @RequestMapping("/")
     public String homeAction(Model model){
+
+        model.addAttribute("institutions", institutionListHomeService.getList());
+
         return "index";
     }
 }

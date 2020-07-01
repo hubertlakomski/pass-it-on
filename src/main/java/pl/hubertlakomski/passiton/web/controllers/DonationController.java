36e2bcf06489c2850caf12a461063c469bdbb6b4
Hubert Łakomski <hubertlakomski@gmail.com>
2020-07-01 21:50:57 +0200

    adding the institution and categories list to the Model in the donation form controller

--- src/main/java/pl/hubertlakomski/passiton/web/controllers/DonationController.java
+++ src/main/java/pl/hubertlakomski/passiton/web/controllers/DonationController.java
@@ -2,11 +2,23 @@ package pl.hubertlakomski.passiton.web.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import pl.hubertlakomski.passiton.service.donation.add.DonationAddService;
 
 @Controller("/donation")
 @RequiredArgsConstructor
 public class DonationController {
 
+    private final DonationAddService donationAddService;
 
+    @GetMapping
+    public String prepareFormPage(Model model){
+
+        model.addAttribute("categories", donationAddService.getListOfCategories());
+        model.addAttribute("institutions", donationAddService.getListOfInstitutions());
+
+        return "form";
+    }
 
 }

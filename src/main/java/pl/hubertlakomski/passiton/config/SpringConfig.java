4add25afb90a6b0797fcd6ce58635c50670029e3
Hubert Łakomski <hubertlakomski@gmail.com>
2020-07-09 18:55:10 +0200

    adding spring config with modelMapper bean

---
+++ src/main/java/pl/hubertlakomski/passiton/config/SpringConfig.java
@@ -0,0 +1,15 @@
+package pl.hubertlakomski.passiton.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+
+        return new ModelMapper();
+    }
+}

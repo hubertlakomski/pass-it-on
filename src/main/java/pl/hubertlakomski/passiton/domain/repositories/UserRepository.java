70d068c1952865838a943c746d97fd706b332eae
Hubert Łakomski <hubertlakomski@gmail.com>
2020-07-12 15:55:24 +0200

    add query to get User with roles

--- src/main/java/pl/hubertlakomski/passiton/domain/repositories/UserRepository.java
+++ src/main/java/pl/hubertlakomski/passiton/domain/repositories/UserRepository.java
@@ -1,7 +1,14 @@
 package pl.hubertlakomski.passiton.domain.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.hubertlakomski.passiton.domain.models.security.User;
 
+import java.util.List;
+
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u INNER JOIN u.roles")
+    List<User> findAllWithRoles();
 }

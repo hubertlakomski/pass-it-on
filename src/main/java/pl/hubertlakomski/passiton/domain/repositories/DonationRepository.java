eebd4bb2ec90e153c389224b9ab47e8a806bd8a8
Hubert Łakomski <hubertlakomski@gmail.com>
2020-07-12 16:41:51 +0200

    add query to get all donations by user id

--- src/main/java/pl/hubertlakomski/passiton/domain/repositories/DonationRepository.java
+++ src/main/java/pl/hubertlakomski/passiton/domain/repositories/DonationRepository.java
@@ -3,5 +3,10 @@ package pl.hubertlakomski.passiton.domain.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.hubertlakomski.passiton.domain.models.Donation;
 
+import java.util.List;
+
 public interface DonationRepository extends JpaRepository<Donation, Long> {
+
+    List<Donation> findAllByDonatingUserId(Long id);
+
 }
